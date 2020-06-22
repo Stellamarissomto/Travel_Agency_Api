0c893afcad0b14e5a0c2f77044d1858ba08d51e9
Stellamarissomto <stellamarissomto@gmail.com>
2020-06-22 16:06:23 +0100

    updated tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,7 +5,7 @@ const Tour = require("../model/tour");
 exports.creatTour = async(req, res) => {
     try {
 
-        const newTour = await Tour.create(req.body);
+       const newTour = await Tour.create(req.body);
         res
         .status(201)
         .json({ status: 'success', data: newTour});
